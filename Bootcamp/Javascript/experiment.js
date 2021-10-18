9ec328d48be20005a42589d1e9561fe022210e6a
Hersonmei <herson.me@hotmail.com>
2021-10-18 16:05:39 -0300

    Finalização

--- Bootcamp/Javascript/experiment.js
+++ Bootcamp/Javascript/experiment.js
@@ -1,5 +1,10 @@
-const monthlyRent = 500;
+let number = Math.floor(Math.random() * 2);
 
-const yearlyRent = monthlyRent * 12;
+let guess = prompt('Escolhe um número de 1 a 3');
 
-console.log(yearlyRent)
+while (guess !== number) {
+    guess = prompt("Ainda não acertou")
+    if (guess === number) break;
+}
+
+console.log("Parabéns! Você acertou!");
